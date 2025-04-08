204e5507bdcda671955b5d5ebc4c0a3c37a35c46
Denver Cope <Denver.cope@gmail.com>
2025-04-09 08:50:30 +1000

    fixing my structure and finalizing

---
+++ methods/Astar.py
@@ -0,0 +1,15 @@
+import networkx as nx 
+import matplotlib.pyplot as plt
+from utils.heuristics.py import euclidean_distance
+from utils.graph import neighbors
+
+
+class Astar:
+    def Search(Self, Graph, Origin, Destinations ):
+
+        target = list(destinations.keys())[0]
+
+    OpenSet = PriorityQueue()
+    print(neighbors)
+
+
